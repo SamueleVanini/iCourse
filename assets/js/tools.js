de71b067370728d0214522545ae99cf4a4c76d08
manuelbarusco <barusco.manuel@gmail.com>
2018-05-02 16:44:16 +0200

    Creata la pagina dinamica per la visualizzazione dei dettagli dei corsi

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -31,6 +31,25 @@ function createSocialBox(comunicazioni){
 }//createActivityBox
 
 /**
+ * Funzione che compila la pagina delle activity
+ * @param informazioni oggetto json con le informazioni dell'activiy
+*/
+function createActivityPage(informazioni){
+   document.getElementById('activity_image').src="data:image/jpeg;base64,"+informazioni[0]["ImmAnteprima"];
+   document.getElementById('title').innerHTML = informazioni[0].NomeCorso;
+   document.getElementById('activity_description').innerHTML = informazioni[0].Descrizione;
+   var testo=""; //stampa momenti dell'activity
+   for(var i=0;i<informazioni[1].length;i++)
+       testo+="<a href='#' class='list-group-item list-group-item-action'>Luogo:"+informazioni[1][i].Luogo+" Data: "+informazioni[1][i].Data+" Orario: "+informazioni[1][i].OraInizio+" - "+informazioni[1][i].OraFine+"</a>";
+   document.getElementById('activity_moments').innerHTML = testo;
+   var testo="Insegnanti responsabili del corso:<ul>"; //stampa insegnanti responsabili dell'activity
+   for(var i=0;i<informazioni[2].length;i++)
+       testo+="<li>"+informazioni[2][i].Nome+" "+informazioni[2][i].Cognome+"</li>";
+   testo+="</ul>";
+   document.getElementById('activity_spec').innerHTML = testo;
+}//createActivityPage
+
+/**
  * Funzione che mostra o nasconde il form di inserimento eventi.
 */
 function showForm(){
@@ -52,7 +71,7 @@ function showFine(){
     }else{
         document.getElementById('div-fine-rip').style.display = "none";
     }
-}//showFine 
+}//showFine
 
 function valoreDaId(id){
     return document.getElementById(id).value;
@@ -64,37 +83,37 @@ function valoreDaId(id){
  */
 function creaFormatoRichiesta(){
     var dati = [];
-    
+
     var oggetto = new Object();
     oggetto.name = 'nomeEvento';
     oggetto.value = valoreDaId('nomeEvento');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'dataInizioEvento';
     oggetto.value = valoreDaId('dataInizioEvento');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'oraInizioEvento';
     oggetto.value = valoreDaId('oraInizioEvento');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'dataFineEvento';
     oggetto.value = valoreDaId('dataFineEvento');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'oraFineEvento';
     oggetto.value = valoreDaId('oraFineEvento');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'ripetizione';
     oggetto.value = valoreDaId('ripetizione');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'fineRipetizione';
     oggetto.value = valoreDaId('fineRipetizione');
@@ -104,18 +123,18 @@ function creaFormatoRichiesta(){
     oggetto.name = 'luogo';
     oggetto.value = valoreDaId('luogo');
     dati.push(oggetto);
-    
+
     var oggetto = new Object();
     oggetto.name = 'descr';
     oggetto.value = valoreDaId('descr');
     dati.push(oggetto);
-    
+
     return dati;
 }//creaFormatoRichiesta
 
 function sendDatiCorso(){
     dati = creaFormatoRichiesta();
-    
+
     var callback = (err, res)=>{
         if(err){
             console.log("Errore: " + err + "; status: " + res);
@@ -123,7 +142,7 @@ function sendDatiCorso(){
             console.log("bella");
         }
     }//callback
-    
+
     console.log(dati);
     var richiesta = new Request("/iCourse/src/controller/new_course_controller.php", "POST", dati, callback);
     richiesta.send();
