a7afa93b94227d97261bd3b149ef225dfbca8c2c
BernGrill <albertogross99@gmail.com>
2018-05-04 19:15:05 +0200

    footer sistemato

--- template/footer.php
+++ template/footer.php
@@ -1,14 +1,21 @@
 <footer class="text-muted">
     <div class="container">
         <p class="float-right">
-            <a href="#" id="return-to-top">
-                <i class="fa fa-chevron-up">
-                    <svg fill="white" height="35" viewBox="0 0 24 24" width="35" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                    </svg>
-                </i>
-            </a>
+			<a href="#" id="return-to-top">
+				<i class="fa fa-chevron-up"> 
+					<svg fill="white" height="35" viewBox="0 0 24 24" width="34" xmlns="http://www.w3.org/2000/svg" style="display: block; margin: auto;">
+						<path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
+						<path d="M0 0h24v24H0z" fill="none"/>
+					</svg>
+				</i>
+			</a>
         </p>
+    
+    <div class="container-new row">
+        <div class="col-md-12" id="foot">
+            <p>Autori: Samuele Vanini, Manuel Barusco, Riccardo Bonavigo, Andrea Tessari, Mattia Berin, Antonio Varotto, Andrea Paccagnella, Leonardo Serrentino, Alberto Grosselle</p>
+            <p>Istituto Tecnico Industriale "Francesco Severi" Via Luigi Pettinati, 46 - 35129 - Padova  </p>
+        </div>
+    </div>
     </div>
 </footer>
