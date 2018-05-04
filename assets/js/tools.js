efff99c3354b315dbee7ef69459ca5acfb1df11e
Samuele <samuele.vanini@live.it>
2018-05-04 20:55:08 +0200

    Fix visualizzazione materiale

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -90,7 +90,7 @@ function createActivityPage(informazioni){
         testo+="<li>"+informazioni[2][i].Nome+" "+informazioni[2][i].Cognome+"</li>";
     testo+="</ul>";
     document.getElementById('activity_spec').innerHTML = testo;
-    if(informazioni[3].nomeMateriale){
+    if(informazioni[3][0]["NomeMateriale"]){
         testo='<table class="table table-hover" id="tabellaFile"><thead><tr><th scope="col">#</th><th scope="col">Nome file</th><th scope="col">Data aggiunta</th></tr></thead>';
         for(var i=0;i<informazioni[3].length;i++)
             testo+="<tr><th>"+(i+1)+"</th><td>"+informazioni[3][i].NomeMateriale+"</td><td>"+informazioni[3][i].DataAggiunta+"</td></tr>";
